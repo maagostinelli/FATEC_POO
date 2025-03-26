a5148414f5ddecf1e332cdd227f0e8b2de8eb436
Mariana A. Agostinelli <81495551+maagostinelli@users.noreply.github.com>
2025-03-26 19:45:52 -0300

    Update Main.java

--- clinica/Main.java
+++ clinica/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         // Criando um paciente
-        Paciente paciente = new Paciente("João Silva", "12345678900", "99999-9999", "Masculino", 30);
+        Paciente paciente = new Paciente("João Silva", "12345678900", "99456-9899", "Masculino", 30);
         paciente.cadastrar();
         
         // Criando um recepcionista
-        Recepcionista recepcionista = new Recepcionista("Ana Souza", "88888-8888", "senha123", "98765432100");
+        Recepcionista recepcionista = new Recepcionista("Ana Luiza Abreu", "8678-9941", "senha123", "98765432100");
         
         // Criando um médico
-        Medico medico = new Medico("Dr. Carlos", "77777-7777", "senha456", "CRM12345", "Cardiologia");
+        Medico medico = new Medico("Dr. Carlos", "7457-1237", "senha456", "CRM12995", "Cardiologia");
 
         // Criando uma consulta
         Consulta consulta = new Consulta("Dor no peito", "Histórico de pressão alta");
